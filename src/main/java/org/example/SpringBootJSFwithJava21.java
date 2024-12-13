3282a1bc108ccdef79bf4bb6832f85823c106a2b
İbrahim Bolat <bolat6606@hotmail.com>
2024-12-13 15:20:16 +0300

    13.12.2024ikindi

--- src/main/java/org/example/SpringBootJSFwithJava21.java
+++ src/main/java/org/example/SpringBootJSFwithJava21.java
@@ -1,10 +1,7 @@
 package org.example;
 
-import org.example.config.JsfInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -14,8 +11,4 @@ public class SpringBootJSFwithJava21 {
         SpringApplication.run(SpringBootJSFwithJava21.class, args);
     }
 
-    @Bean
-    public ServletContextInitializer facesInitializer() {
-        return new JsfInitializer();
-    }
 }
